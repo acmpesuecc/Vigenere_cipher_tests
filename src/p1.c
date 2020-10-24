286ed3bd880df0cd5dc7deb2f070ed7177af8c0c
pallaviiprabhu <69977387+pallaviiprabhu@users.noreply.github.com>
2020-10-24 20:47:36 +0530

    Dynamically allocated the array

--- src/p1.c
+++ src/p1.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main()
 {
-        char msg[300], key[300];
-        printf("Enter the message to encrypt in caps: \n");
-        scanf("%[^\n]",msg);
-
+        //char msg[300], key[300]
+        char *msg = NULL;
+	      char *key = NULL;
+    
+	      int n, k;
+	      printf("Enter the number of messages you want to encrypt\n");
+	      scanf("%d",&n);
+	      msg = (char*)malloc(n*sizeof(char));
+	      key = (char*)malloc(n*sizeof(char));
+	      for (k=0; k<n; ++k){
+		      printf("Enter the message to encrypt\n");
+		      scanf("%s",&msg[k]);
+	      	printf("Enter the key to use in caps and no spaces:\n");
+	      	scanf("%s",&key[k]);
+        }
         char new_msg[300];
+        
         int c = 0, d = 0;
         int arr[300];
+
+        
         while (msg[c] != '\0')
         {
 
@@ -20,10 +35,9 @@ int main()
           c++;
         }
         new_msg[d] = '\0';
-
-        printf("Enter the key to use in caps and no spaces:\n");
-        scanf("%s",key);
-
+      
+      for(k=0;k<n;k++)
+      {
     	int msgLen = strlen(new_msg), keyLen = strlen(key), i, j;
     	char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
     	//generating new key
@@ -49,5 +63,7 @@ int main()
     	printf("\nNew Generated Key: %s", newKey);
     	printf("\nEncrypted Message: %s", encryptedMsg);
     	printf("\nDecrypted Message: %s\n", decryptedMsg);
+      }
 	return 0;
+        
 }
